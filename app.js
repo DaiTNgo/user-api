d38681060098998054684f72a770993aeced36ed
daingo <dnt7200@gmail.com>
2021-09-24 17:12:19 +0700

    c1

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const apiPath = '/api/';
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// website
+app.use(express.static('client'));
+
+// routers
+app.use(apiPath + 'users', require('./routes/users.route'));
+// app.use(apiPath + 'products', require('./routes/products.route'));
+
+app.listen(port, function () {
+	const host = 'localhost';
+	console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ controllers/user.controller.js
@@ -0,0 +1,23 @@
+const User = require('../models/user.model');
+const people = require('../assets/people.json');
+
+class UserController {
+	get(req, res) {
+		// return res.status(200).json('Chào bạn, đã nhận được yêu cầu của bạn !');
+		const filter = req.query.filter;
+		// console.log('filter', filter);
+		const filterPeople = people.filter((person) =>
+			person.first_name.includes(filter)
+		);
+		return res
+			.status(200)
+			.json({ data: filterPeople, length: filterPeople.length });
+	}
+
+	post(req, res) {
+		const filter = req.body.filter;
+		return res.status(200).json({ result: `Chào bạn abc` });
+	}
+}
+
+module.exports = new UserController();
